418e5df38269e1e72e7b3099346e63bb8f5240f2
Reggielang <37894298+Reggielang@users.noreply.github.com>
2021-07-08 17:35:43 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void main(String[] args){
+        int mynumber = 1002;
+        System.out.println(mynumber);
+    }
+}
+
+//变量的使用
+//1 Java定义变量的格式 数据类型 变量名 = 变量值;
+//2  变量必须先声明后使用
+class VariableTest{
+    public static void main(String[] args){
+        //变量的定义
+        int myAge =12;
+        //变量的使用
+        System.out.println(myAge);
+        int myNumber = 1001;
+    }
+}
